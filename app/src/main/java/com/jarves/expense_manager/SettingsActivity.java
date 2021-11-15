d7007dad44ecf80523d087df9248e93fa7db8964
raghul <raghuls72001@gmail.com>
2021-11-15 20:58:34 +0530

    dark mode

--- app/src/main/java/com/jarves/expense_manager/SettingsActivity.java
+++ app/src/main/java/com/jarves/expense_manager/SettingsActivity.java
@@ -1,14 +1,44 @@
 package com.jarves.expense_manager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    ToggleButton tb;
+
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.darkTheme);
+        } else {
+            setTheme(R.style.AppTheme);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_two);
+        tb=findViewById(R.id.tb1);
+
+        tv=findViewById(R.id.tv);
+        tb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                }
+                else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+            }
+        });
     }
 }
